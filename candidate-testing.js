820b69b4411a09c565d0dac9a5c2c650498a854c
Carly Langlois <cj.langlois.cl@gmail.com>
2020-12-21 11:51:53 -0600

    actually add jasmine this time

---
+++ candidate-testing.js
@@ -0,0 +1,13 @@
+let questions = [
+   "True or false: 5 kilometer == 5000 meters?",
+   "(5 + 3)/2 * 10 = ?",
+   'Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2?',
+   "Who was the first American woman in space?",
+   "What is the minimum crew size for the ISS?"
+];
+
+// Don't write any code below this line //
+// And don't change these or your program will not run as expected //
+module.exports = {
+   questions: questions,
+};
---
+++ spec/candidate-testing.spec.js
@@ -0,0 +1,12 @@
+const solution = require('../candidate-testing');
+
+describe("Candidate Testing solution", function() {
+
+	// questions tests //
+	it("questions array contains 5 questions", function() {
+		expect(solution.questions.length).toBe(5);
+	});
+
+	
+ 
+ });
