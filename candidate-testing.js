89a6441daa641a3420d2adc82570929be3c5b6f5
Carly Langlois <cj.langlois.cl@gmail.com>
2021-01-15 11:07:37 -0600

    add autograding starter code

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,13 +1,58 @@
-let questions = [
-   "True or false: 5 kilometer == 5000 meters?",
-   "(5 + 3)/2 * 10 = ?",
-   'Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2?',
-   "Who was the first American woman in space?",
-   "What is the minimum crew size for the ISS?"
-];
+const input = require('readline-sync');
+
+// TODO 2: modify your quiz app to ask 5 questions //
+
+// TODO 1.1a: Define candidateName // 
+let candidateName;
+// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
+let question;
+let correctAnswer;
+let candidateAnswer;
+let questions;
+let correctAnswers;
+let candidateAnswers;
+
+
+function askForName() {
+  // TODO 1.1b: Ask for candidate's name //
+
+}
+
+function askQuestion() {
+  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+
+
+}
+
+function gradeQuiz(candidateAnswers) {
+
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+
+
+  let grade;
+  
+
+  return grade;
+}
+
+function runProgram() {
+  askForName();
+  // TODO 1.1c: Ask for candidate's name //
+  
+  askQuestion();
+  gradeQuiz(this.candidateAnswers);
+}
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
 module.exports = {
-   questions: questions,
+  candidateName: candidateName,
+  question: question,
+  correctAnswer: correctAnswer,
+  candidateAnswer: candidateAnswer,
+  questions: questions,
+  correctAnswers: correctAnswers,
+  candidateAnswers: candidateAnswers,
+  gradeQuiz: gradeQuiz,
+  runProgram: runProgram
 };
